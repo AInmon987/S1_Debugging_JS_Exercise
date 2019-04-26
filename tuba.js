41e793bcc318cd36aef792904472f96e9b4a1ecc
austin inmon <Ainmon987@west-mec.org>
2019-04-26 13:27:43 -0700

    Final Commit
    Finished debugging the code and got the program to correctly display the tractor to the user.

--- tuba.js
+++ tuba.js
@@ -23,7 +23,7 @@ var messageElement = document.getElementById("message");
 var acresFieldset = document.getElementsByTagName("fieldset")[0];
 var cropsFieldset = document.getElementsByTagName("fieldset")[1];
 var monthsFieldset = document.getElementsByTagName("fieldset")[2];
-var fuelFieldset = document.getElementsByTagName("fieldset)[3]");
+var fuelFieldset = document.getElementsByTagName("fieldset")[3];
 
 /* global variables referencing text input elements */
 var monthsBox = document.forms[0].months;
@@ -31,18 +31,53 @@ var acresBox = document.forms[0].acres;
 
 /* verify acres text box entry is a positive number */
 function verifyAcres() {
-    testFormCompleteness();
+    var validity = true;
+    var messageText = "";
+    try {
+        if (!(acresBox.value > 0)) {
+            throw "Please enter a number of acres greater    than 0.";
+        }
+    } catch (message) {
+        validity = false;
+        messageText = message;
+        // remove erroneous entry from input box
+        acresBox.value = "";
+    } finally {
+        acresComplete = validity;
+        // remove former recommendation
+        messageElement.innerHTML = messageText;
+        messageHeadElement.innerHTML = "";
+        testFormCompleteness();
+    }
+
 }
 
 /* verify at least one crops checkbox is checked */
 function verifyCrops() {
     testFormCompleteness();
 }
-
 /* verify months text box entry is between 1 and 12 */
 function verifyMonths() {
-    testFormCompleteness();
+    var validity = true;
+    var messageText = "";
+    try {
+        if (!(monthsBox.value >= 1 && monthsBox.value <= 12)) {
+            throw "Please enter a number of months between 1 and 12.";
+        }
+    } catch (message) {
+        validity = false;
+        messageText = message;
+        // remove erroneous entry from input box
+        monthsBox.value = "";
+    } finally {
+        monthsComplete = validity;
+        // remove former recommendation
+        messageElement.innerHTML = messageText;
+        messageHeadElement.innerHTML = "";
+        testFormCompleteness();
+    }
 }
+
 
 /* verify that a fuel option button is selected */
 function verifyFuel() {
